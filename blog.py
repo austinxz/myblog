d517b07f59a2d2449568614ef02df5989ac7035a
austin <austinxz650@gmail.com>
2013-12-16 16:57:31 -0500

    change identifier

--- blog.py
+++ blog.py
@@ -4,6 +4,7 @@ import urllib
 import datetime
 import unicodedata
 import time
+import re
 
 from google.appengine.api import users
 from google.appengine.ext import db
@@ -16,18 +17,43 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+
+class Page():
+	previousPage = 1
+	currentPage = 1
+	nextPage = 1
+
 # Class used to represent a blog post object.
 class Post(db.Model):
-	author = db.StringProperty(required = True)
+	postId = db.StringProperty(required = True)
+	author = db.StringProperty(required = True, indexed = True)
 	blogName = db.StringProperty(required = True, indexed = True)
+	blogId = db.StringProperty(required = True)
 	title = db.StringProperty(required = True, indexed = True)
 	content = db.TextProperty(required = True, indexed = False)
 	createTime = db.DateTimeProperty(indexed=True)
 	modifiedTime = db.DateTimeProperty()
-	tags = db.StringListProperty(indexed = True, default = None)
+	tags = db.StringListProperty(default=None, indexed = True)
+
+	def contentParser(self):
+		result = re.sub(r'(http[s]?://[^,\s\n]*)', '<a href="\\1">\\1</a>', self.content)
+		result = re.sub(r'<a href="(http[s]?://[^,\s\n]*[(.jpg)(.png)(.gif)])">.*</a>', '<img src="\\1" width="300" height="300">', result)
+		result = result.replace('\n', '<br />')
+		return result
+	
+	def contentPreview(self):
+		_content = self.contentParser()
+		return _content[0 : 499]
+
+	def tagsStr(self):
+		tags = ""
+		for tag in self.tags:
+			tags = tags + tag + ","
+		return tags[:len(tags) - 1]
 
 # Class used to represent the blog object.
 class Blog(db.Model):
+	blogId = db.StringProperty(required = True)
 	author = db.StringProperty(required = True)
 	blogName = db.StringProperty(required = True, indexed=True)
 	createTime = db.DateTimeProperty(required = True, indexed=True)
@@ -37,27 +63,66 @@ class Blog(db.Model):
 class MainPage(webapp2.RequestHandler):
 	
 	def get(self):
+		currentPageNum = int(self.request.get('currentPageNum', "1"))
+
 		if users.get_current_user():
 			# 1. List all the blogs created by the user
 			userEmail = users.get_current_user().email()
 			blogs = db.GqlQuery("SELECT * FROM Blog WHERE author = :1 ORDER BY createTime DESC LIMIT 10", userEmail)	
 			othersBlogs = db.GqlQuery("SELECT * FROM Blog WHERE author != :1 LIMIT 10", userEmail)
-			posts = list()
+			posts = db.GqlQuery("SELECT * FROM Post ORDER BY createTime DESC")
 			url = users.create_logout_url(self.request.uri)
 			url_linktext = 'Logout'
 		else:
 			userEmail = None
 			blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY createTime DESC LIMIT 10")
 			othersBlogs = list()
-			posts = db.GqlQuery("SELECT * FROM Post ORDER BY createTime DESC LIMIT 10")
+			posts = db.GqlQuery("SELECT * FROM Post ORDER BY createTime DESC")
 			url = users.create_login_url(self.request.uri)
 			url_linktext = 'Login'
 
+		_tags = set()
+		for post in posts:
+			for tag in post.tags:
+				_tags.add(tag)
+
+		tags = list(_tags)
+		tags.sort()
+
+		# At most 10 pages at the home page.
+		# 1. Get the total number of page
+		if posts.get():
+			postsNum = 0
+			for _post in posts:
+				postsNum = postsNum + 1
+		else:
+			postsNum = 0
+
+		# 2. Create a page object, and fill in the requested attributes.
+		page = Page()
+		# 2.1 Current page
+		page.currentPage = currentPageNum
+		# 2.2 Previous page
+		if currentPageNum == 1:
+			page.previousPage = None
+		else:
+			page.previousPage = currentPageNum - 1
+		# 2.3 Next page
+		if currentPageNum * 10 + 1 > postsNum:
+			page.nextPage = None
+		else:
+			page.nextPage = currentPageNum + 1
+
+		# 3. Compute the posts for current page.
+		postsList = posts[(currentPageNum - 1)* 10 : min(currentPageNum * 10, postsNum)]
+
 		template_values = {
+			'page': page,
 			'user': users.get_current_user(),
 			'blogs': blogs,
 			'othersBlogs':othersBlogs,
-			'posts': posts,
+			'posts': postsList,
+			'tags': tags,
 			'url': url,
 			'url_linktext': url_linktext,
 		}
@@ -68,8 +133,10 @@ class MainPage(webapp2.RequestHandler):
 class CreatePostView(webapp2.RequestHandler):
 
 	def get(self):
+		blogId = self.request.get('blogId')
+		blog = db.GqlQuery("SELECT * FROM Blog WHERE blogId = :1", blogId)
 		template_values = {
-			'blogName': self.request.get('blogName'),
+			'blog': blog.get(),
 		}
 
 		template = JINJA_ENVIRONMENT.get_template('createPost.html')
@@ -87,35 +154,45 @@ class CreatePostDeal(webapp2.RequestHandler):
 
 	def post(self):
 		# Create new post
-		
-		post = Post(author = users.get_current_user().email(),
-					blogName = self.request.get('blogName'),
-					title = self.request.get('title'),
-					content = self.request.get('content'),
+		blogId = self.request.get('blogId')
+		title = self.request.get('title')
+		content = self.request.get('content')
+		tags = self.request.get('tags')
+
+		blog = db.GqlQuery("SELECT * FROM Blog WHERE blogId = :1", blogId)
+		postId = users.get_current_user().email() + str(datetime.datetime.now())
+		post = Post(postId = postId,
+					author = users.get_current_user().email(),
+					blogName = blog.get().blogName,
+					blogId = blog.get().blogId,
+					title = title,
+					content = content,
 					createTime = datetime.datetime.now(),
 					modifiedTime = datetime.datetime.now())
 
-		preTags = self.request.get('tags')
-		if len(preTags) > 0 :
-			tags = preTags.split(',')
+		if len(tags) > 0 :
+			tags = tags.split(',')
 		else:
 			tags = list()
 		post.tags = tags
 		post.put()
 
 		time.sleep(1)
-		query_params = {'blogName' : self.request.get('blogName'), 'title': self.request.get('title')}
+		query_params = {'postId': postId }
 		self.redirect('/viewPost?' + urllib.urlencode(query_params))
 
-		
 
 # The createBlog which to handle the create_blog operation
 class CreateBlogDeal(webapp2.RequestHandler):
 
 	def post(self):
 		# Create new post
-		blog = Blog(author = users.get_current_user().email(),
-					blogName = self.request.get('blogName'),
+		blogName = self.request.get('blogName')
+
+		blogId = users.get_current_user().email() + str(datetime.datetime.now())
+		blog = Blog(blogId = blogId,
+					author = users.get_current_user().email(),
+					blogName = blogName,
 					createTime = datetime.datetime.now(),
 					modifiedTime = datetime.datetime.now())
 		blog.put()
@@ -127,46 +204,70 @@ class CreateBlogDeal(webapp2.RequestHandler):
 class ViewBlog(webapp2.RequestHandler):
 
 	def get(self):
-		blogName = self.request.get('blogName')
-		author = self.request.get('author')
-		blog = db.GqlQuery("SELECT * FROM Blog WHERE blogName=:1 AND author=:2", blogName, author)
-		posts = db.GqlQuery("SELECT * FROM Post WHERE blogName = :1 ORDER BY createTime DESC LIMIT 10", blogName)
-		if users.get_current_user() and author == users.get_current_user().email():
-			edit = True
-		else:
-			edit = False
-		template_values = {
-			'blog': blog.get(),
-			'user': users.get_current_user(),
-			'edit': edit,
-			'posts': posts,
-		}
+		blogId = self.request.get('blogId')
+		currentPageNum = int(self.request.get('currentPageNum', "1"))
 
-		template = JINJA_ENVIRONMENT.get_template('viewBlog.html')
-		self.response.write(template.render(template_values))
-
-class ViewPost(webapp2.RequestHandler):
-
-	def get(self):
-		blogName = self.request.get('blogName')
-		print("******" + blogName)
-		title = self.request.get('title')
-		blog = db.GqlQuery("SELECT * FROM Blog WHERE blogName=:1", blogName)
-		post = db.GqlQuery("SELECT * FROM Post WHERE blogName=:1 AND title=:2", blogName, title)
+		blog = db.GqlQuery("SELECT * FROM Blog WHERE blogId=:1", blogId)
+		posts = db.GqlQuery("SELECT * FROM Post WHERE blogId = :1 ORDER BY createTime DESC", blogId)
 		if users.get_current_user() and blog.get().author == users.get_current_user().email():
 			edit = True
 		else:
 			edit = False
 
+
+		# At most 10 pages at the home page.
+		# 1. Get the total number of page
+		if posts.get():
+			postsNum = 0
+			for _post in posts:
+				postsNum = postsNum + 1
+		else:
+			postsNum = 0
+
+		# 2. Create a page object, and fill in the requested attributes.
+		page = Page()
+		# 2.1 Current page
+		page.currentPage = currentPageNum
+		# 2.2 Previous page
+		if currentPageNum == 1:
+			page.previousPage = None
+		else:
+			page.previousPage = currentPageNum - 1
+		# 2.3 Next page
+		if currentPageNum * 10 + 1 > postsNum:
+			page.nextPage = None
+		else:
+			page.nextPage = currentPageNum + 1
+
+		# 3. Compute the posts for current page.
+		postsList = posts[(currentPageNum - 1)* 10 : min(currentPageNum * 10, postsNum)]
+
 		template_values = {
-			'author': users.get_current_user().email(),
-			'blogName': blogName,
+			'blog': blog.get(),
+			'page': page,
 			'edit': edit,
-			'title': post.get().title,
-			'content': post.get().content.replace('\n', '<br />'),
-			'createTime': post.get().createTime,
-			'modifiedTime': post.get().modifiedTime,
-			'tags': post.get().tags,
+			'posts': postsList,
+		}
+
+		template = JINJA_ENVIRONMENT.get_template('viewBlog.html')
+		self.response.write(template.render(template_values))
+
+
+
+class ViewPost(webapp2.RequestHandler):
+
+	def get(self):
+		postId = self.request.get('postId')
+
+		post = db.GqlQuery("SELECT * FROM Post WHERE postId = :1", postId)
+		if users.get_current_user() and post and post.get().author == users.get_current_user().email():
+			edit = True
+		else:
+			edit = False
+
+		template_values = {
+			'edit': edit,
+			'post': post.get(),
 		}
 
 		template = JINJA_ENVIRONMENT.get_template('viewPost.html')
@@ -175,13 +276,14 @@ class ViewPost(webapp2.RequestHandler):
 class DeleteBlog(webapp2.RequestHandler):
 
 	def get(self):
-		blogName = self.request.get('blogName')
+		blogId = self.request.get('blogId')
+
 		# 1. Delete the blog in blog table
-		blog = db.GqlQuery("SELECT * FROM Blog WHERE blogName = :1 AND author = :2", blogName, users.get_current_user().email())
+		blog = db.GqlQuery("SELECT * FROM Blog WHERE blogId = :1", blogId)
 		db.delete(blog)
 
 		#2. Delete all the post which blogs to that blog.
-		posts = db.GqlQuery("SELECT * FROM Post WHERE blogName = :1 AND author = :2", blogName, users.get_current_user().email())
+		posts = db.GqlQuery("SELECT * FROM Post WHERE blogId = :1", blogId)
 		for post in posts:
 			db.delete(post)
 
@@ -192,28 +294,24 @@ class DeleteBlog(webapp2.RequestHandler):
 class DeletePost(webapp2.RequestHandler):
 
 	def get(self):
-		author = self.request.get('author')
-		title = self.request.get('title')
-		blogName = self.request.get('blogName')
+		postId = self.request.get('postId')
 
-		posts = db.GqlQuery("SELECT * FROM Post WHERE author=:1 AND title=:2 AND blogName=:3",
-			author, title, blogName)
-		for post in posts:
-			db.delete(post)
+		post = db.GqlQuery("SELECT * FROM Post WHERE postId = :1", postId)
+		blogId = post.get().blogId
+		for p in post:
+			db.delete(p)
 
 		time.sleep(1)
-		query_params = {'blogName' : self.request.get('blogName'), 'author': author}
+		query_params = {'blogId': blogId}
 		self.redirect('/viewBlog?' + urllib.urlencode(query_params))
 
 
 class EditPost(webapp2.RequestHandler):
 
 	def get(self):
-		author = self.request.get('author')
-		title = self.request.get('title')
-		blogName = self.request.get('blogName')
-		post = db.GqlQuery("SELECT * FROM Post WHERE author=:1 AND title=:2 AND blogName=:3",
-			author, title, blogName)
+		postId = self.request.get('postId')
+
+		post = db.GqlQuery("SELECT * FROM Post WHERE postId = :1", postId)
 
 		template_values = {
 			'post': post.get(),
@@ -225,31 +323,65 @@ class EditPost(webapp2.RequestHandler):
 	def post(self):
 		newTitle = self.request.get('new_title')
 		newContent = self.request.get('new_content')
-		blogName = self.request.get('blogName')
-		oldTitle = self.request.get('oldTitle')
+		postId = self.request.get('postId')
+		tags = self.request.get('new_tags')
 
-		post = db.GqlQuery("SELECT * FROM Post WHERE author=:1 AND blogName=:2 AND title=:3",
-			users.get_current_user().email(), blogName, oldTitle)
+		if tags:
+			newTags = tags.split(',')
+		else:
+			newTags = list()
+		post = db.GqlQuery("SELECT * FROM Post WHERE postId = :1", postId)
 
 		for p in post:
 			p.title = newTitle
 			p.content = newContent
 			p.modifiedTime = datetime.datetime.now()
+			p.tags = newTags
 			db.put(p)
 
 		time.sleep(1)
-		query_params = {'blogName' : self.request.get('blogName'), 'title': newTitle}
+		query_params = {'postId': postId }
 		self.redirect('/viewPost?' + urllib.urlencode(query_params))
 
 class TagPosts(webapp2.RequestHandler):
 
 	def get(self):
-		tag = self.request.get('t')
-		posts = db.GqlQuery("SELECT * FROM Post WHERE :1 in tags", tag)
+		_tag = self.request.get('tag')
+		currentPageNum = int(self.request.get('currentPageNum', "1"))
+
+		posts = db.GqlQuery("SELECT * FROM Post WHERE tags = :1 ORDER BY createTime DESC", _tag)
+
+		# At most 10 pages at the home page.
+		# 1. Get the total number of page
+		if posts.get():
+			postsNum = 0
+			for _post in posts:
+				postsNum = postsNum + 1
+		else:
+			postsNum = 0
+			
+		# 2. Create a page object, and fill in the requested attributes.
+		page = Page()
+		# 2.1 Current page
+		page.currentPage = currentPageNum
+		# 2.2 Previous page
+		if currentPageNum == 1:
+			page.previousPage = None
+		else:
+			page.previousPage = currentPageNum - 1
+		# 2.3 Next page
+		if currentPageNum * 10 + 1 > postsNum:
+			page.nextPage = None
+		else:
+			page.nextPage = currentPageNum + 1
+
+		# 3. Compute the posts for current page.
+		postsList = posts[(currentPageNum - 1)* 10 : min(currentPageNum * 10, postsNum)]
 
 		template_values = {
-			'tag': tag,
-			'posts': posts,
+			'page': page,
+			'tag': _tag,
+			'posts': postsList
 		}
 
 		template = JINJA_ENVIRONMENT.get_template('tagPosts.html')
